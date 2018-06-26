535861f90893c3a4389857ce325a3f1f41bb5215
Ledio <L.Bode515@gmail.com>
2018-06-26 15:16:25 -0400

    2

--- index.js
+++ index.js
@@ -4,18 +4,37 @@ fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, iteratee) {
+      if(collection instanceof Array === true){
+        for (index = 0; index < collection.length; index++){
+          iteratee(collection[index]);
+        }
+      } else {
+        for (const key in collection){
+          iteratee(collection[key]);
+        }
+      }
+      return collection;
     },
 
-    map: function() {
-
+    map: function(collection, iteratee)  {
+      const newArray = [];
+      if(collection instanceof Array === true){
+        for (index = 0; index < collection.length; index++){
+          newArray.push(iteratee(collection[index]));
+        }
+      } else {
+        for (const key in collection){
+          newArray.push(iteratee(collection[key]));
+        }
+      }
+      return newArray;
     },
 
     reduce: function() {
 
     },
-    
+
     functions: function() {
 
     },
